adc5eff92bb9ad81cf280729a79772b83e6bd3db
ViniBravo <vinicius.t.campos@iesb.edu.br>
2025-09-24 20:52:31 -0300

    resolvendo o index

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
-// TODO: Membro 1 - Importar e mapear rota de alunos
 
-// TODO: Membro 2 - Importar e mapear rota de professores
+
+
+
+const professoresRouter = require('./routes/professores')
+app.use(professoresRouter)
+
+
+
+
+
+const professoresRouter = require('./routes/professores')
+app.use(professoresRouter)
 
 app.listen(3000, () => {
-console.log('Server is running on http://localhost:3000')
-})
+    console.log("Aplicação rodando em http://localhost:3000")
+    })
