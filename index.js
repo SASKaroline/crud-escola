97ee8cfdf4a4890ac21e68443c606b937cb22bbb
ViniBravo <vinicius.t.campos@iesb.edu.br>
2025-09-24 20:57:20 -0300

    resolvendo conflito

--- index.js
+++ index.js
@@ -8,14 +8,18 @@ app.use(express.json())
 
 
 
+
+
+
+
+
+
+
+
+
+
 const professoresRouter = require('./routes/professores')
 app.use(professoresRouter)
-
-
-
-
-
-
 
 app.listen(3000, () => {
     console.log("Aplicação rodando em http://localhost:3000")
